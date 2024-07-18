045bb86d3b5d919a337033ba03f8f6d7e045236a
thestarrynight17 <nafisat.akiyeva@khazar.org>
2024-07-19 01:02:17 +0400

    Changes

--- miniApp/Classroom.cs
+++ miniApp/Classroom.cs
@@ -13,17 +13,30 @@ namespace miniApp
         private int _limit;
         public int Id { get; }
         public string Name { get; set; }
-        public Student[] Students { get; set; }
+        public List<Student> Students { get; set; }
         public ClassroomType Type { get; set; }
         public int StudentLimit
         {
-            get => _limit; set
+            get => _limit;
+            set
             {
                 if (Type == ClassroomType.backend)
                     value = 20;
                 else if (Type == ClassroomType.frontend)
                     value = 15;
             }
+        }
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+        public void RemoveStudent(Student student)
+        {
+            Students.Remove(student);
+        }
+        public Student FindStudentById(int id)
+        {
+            return Students.FirstOrDefault(x => x.Id == id);
         }
     }
 }
---
+++ miniApp/StudentNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniApp
+{
+    public class StudentNotFoundException : Exception
+    {
+        public StudentNotFoundException() { }
+        public StudentNotFoundException(string message) : base(message) { }
+    }
+}
